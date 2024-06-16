9fad368d8c3d241baf6ae1c08e4ff714da67e3ba
Ramis Mukhlisov <ramis5454668@gmail.com>
2024-06-16 16:27:26 +0300

    build: add containerization

--- app.py
+++ app.py
@@ -1,36 +1,34 @@
 from fastapi import FastAPI, UploadFile
 from osgeo import gdal
 import io
-import datetime
 from georeferencing import find_coordinates
-import numpy as np
 
 app = FastAPI()
 
-@app.post("/api/")
+
+@app.post("/api")
 async def add_task(layout_name: str, file: UploadFile):
     content = await file.read()
     file_bytes = io.BytesIO(content)
-    
+
     vsi_filename = f'/vsimem/{file.filename}'
 
     gdal.FileFromMemBuffer(vsi_filename, file_bytes.getvalue())
-    
+
     crop_arr = gdal.Open(vsi_filename).ReadAsArray()[:3, :, :]
 
     layout = gdal.Open(f"layouts/{layout_name}.tif")
 
     geo_corners, start, end = find_coordinates(layout, crop_arr)
-    print(geo_corners)
-    print(start, end)
 
     geo_corners = geo_corners.tolist()
 
-    coordinates = {"ul": f'{geo_corners[0][0]};{geo_corners[0][1]}',
-                    "ur": f'{geo_corners[1][0]};{geo_corners[1][1]}',
-                    "br": f'{geo_corners[2][0]};{geo_corners[2][1]}',
-                    "bl": f'{geo_corners[3][0]};{geo_corners[3][1]}',
-                    }
+    coordinates = {
+        "ul": f'{geo_corners[0][0]};{geo_corners[0][1]}',
+        "ur": f'{geo_corners[1][0]};{geo_corners[1][1]}',
+        "br": f'{geo_corners[2][0]};{geo_corners[2][1]}',
+        "bl": f'{geo_corners[3][0]};{geo_corners[3][1]}',
+    }
     crs = "EPSG:32637"
 
     gdal.Unlink(vsi_filename)
